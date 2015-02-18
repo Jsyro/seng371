d3a6d084a93aee59681cbb7dad77b33b937062ea
Evan Hildebrandt <ehildebr@uvic.ca>
2015-02-18 14:14:42 -0800

    Edited parse..py

--- parse.py
+++ parse.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 	added = 0 
 	delete = 0 
 	modify = 0 
+	diff = 0
 	commitstring =""
 	for line in lines:
 		if line.startswith("A"):
@@ -20,21 +21,20 @@ if __name__ == '__main__':
 			modify = modify + 1
 		else:
 			
-			stats = [added,delete,modify]
+			diff = abs(added - delete)
+			stats = [added,delete,modify, diff]
 			if (added == 0 and delete == 0):
-				x = 0
+				pass
+
 				#print "This commit was an update: " + str(stats)
 			if (added > 5 and delete > 5):
-				print "This commit might be a refactor: " + str(stats) 
+				print "This commit might be a refactor: Added " + str(stats[0]) + ", Deleted " + str(stats[1]) + ", Modified " + str(stats[2]) + ", Diff " + str(stats[3])
 				print commitstring
 			
 			
 				
 			commitstring = line
-			#print "this commit had: " + str(stats) 
-			#print "Added: " + str(added)
-			#print "Deleted: " + str(delete)
-			#print "Modify: " + str(modify)
 			added = 0
 			delete = 0
 			modify = 0
+			diff = 0
